8c4ec87d9fcc6b1fe0f4fd7253c6b5955c861580
Arielle Jimera <arieru.dev@gmail.com>
2025-08-28 00:29:17 +0800

    refactor(utils): centralize constants in constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,13 @@
+class Constants {
+  static const String appName = 'Bazario';
+  static const String apiBaseUrl = 'https://aishki.helioho.st/php_backend/api';
+
+  // Colors
+  static const int primaryColorValue = 0xFF4CAF50;
+
+  // API Endpoints
+  static const String authEndpoint = 'auth.php';
+  static const String eventsEndpoint = 'events.php';
+  static const String productsEndpoint = 'products.php';
+  static const String vendorsEndpoint = 'vendors.php';
+}
