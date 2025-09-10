676c91ca43b9762aaa7bf266afd0c961550838fd
Arielle Jimera <arieru.dev@gmail.com>
2025-09-10 21:45:51 +0800

    chore: initialize classes for vendor docs, shop and notifs

---
+++ lib/screens/vendor/v_my_docs_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VendorMyDocs extends StatelessWidget {
+  const VendorMyDocs({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Docs')),
+      body: const Center(child: Text('My Docs Screen Placeholder')),
+    );
+  }
+}
---
+++ lib/screens/vendor/v_my_shop_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VendorMyShop extends StatelessWidget {
+  const VendorMyShop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('My Shop')),
+      body: const Center(child: Text('My Shop Screen Placeholder')),
+    );
+  }
+}
