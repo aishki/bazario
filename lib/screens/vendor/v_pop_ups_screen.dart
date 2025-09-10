e913a7f6cb838d621373591a55330b8b61f60147
Arielle Jimera <arieru.dev@gmail.com>
2025-09-10 21:46:14 +0800

    chore: initialize classes for vendor pop ups

---
+++ lib/screens/vendor/v_pop_ups_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class VendorPopUps extends StatelessWidget {
+  const VendorPopUps({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pop-Ups')),
+      body: const Center(child: Text('Pop-Ups Screen Placeholder')),
+    );
+  }
+}
