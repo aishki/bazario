7fa159b4b89b5e35ceed32a2c087a9821ad8c3c9
Arielle Jimera <arieru.dev@gmail.com>
2025-10-01 18:22:00 +0800

    chore: initial application of notifs with bg

--- lib/screens/vendor/v_notifs_screen.dart
+++ lib/screens/vendor/v_notifs_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:iconify_flutter/iconify_flutter.dart';
+
 import '../../models/vendor.dart';
+import 'dart:ui';
 
 class VendorNotificationScreen extends StatelessWidget {
   final String userId;
@@ -14,12 +17,61 @@ class VendorNotificationScreen extends StatelessWidget {
     required this.businessName,
     required this.vendor,
   });
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Notifications')),
-      body: const Center(child: Text('Notifications Screen Placeholder')),
+      extendBodyBehindAppBar: true,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white.withOpacity(0.1),
+              automaticallyImplyLeading: false,
+              title: Row(
+                children: [
+                  IconButton(
+                    onPressed: () => Navigator.pop(context),
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Color(0xFF569109),
+                      size: 28,
+                    ),
+                  ),
+                  const Iconify(
+                    "streamline-plump:announcement-megaphone",
+                    size: 24,
+                    color: Color(0xFF569109),
+                  ),
+                  const SizedBox(width: 8),
+                  const Text(
+                    "Notifications",
+                    style: TextStyle(
+                      fontFamily: 'Starla',
+                      fontSize: 22,
+                      color: Color(0xFF569109),
+                    ),
+                  ),
+                ],
+              ),
+              bottom: PreferredSize(
+                preferredSize: const Size.fromHeight(2),
+                child: Container(height: 2, color: const Color(0xFF74CC00)),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("lib/assets/images/notifs-bg.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
