27f0eac69e01a3d633adbc0c0be69d95c8eca815
Arielle Jimera <arieru.dev@gmail.com>
2025-10-09 20:18:01 +0800

    feat: product model

---
+++ lib/models/product.dart
@@ -0,0 +1,42 @@
+class Product {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String? imageUrl;
+  final int stock;
+  final String category;
+  final String? businessPartnerId;
+  final String? addedBy;
+  final DateTime? createdAt;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    this.imageUrl,
+    required this.stock,
+    required this.category,
+    this.businessPartnerId,
+    this.addedBy,
+    this.createdAt,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id']?.toString() ?? '',
+      name: json['name'] ?? '',
+      description: json['description'] ?? '',
+      price: double.tryParse(json['price'].toString()) ?? 0.0,
+      imageUrl: json['image_url'],
+      stock: int.tryParse(json['stock']?.toString() ?? '0') ?? 0,
+      category: json['category'] ?? 'others',
+      businessPartnerId: json['business_partner_id'],
+      addedBy: json['added_by'],
+      createdAt: json['created_at'] != null
+          ? DateTime.tryParse(json['created_at'])
+          : null,
+    );
+  }
+}
